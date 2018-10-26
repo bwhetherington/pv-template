36e7b99be16383fce6ef8b04a356c065b069e9d1
Ben Hetherington <benhetherington@Bens-MacBook-Pro.local>
2018-10-26 17:00:11 +0200

    Add Google Maps support

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,13 +3,25 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { object } from 'prop-types';
 import { Paper, Typography } from '@material-ui/core';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
 
 const styles = theme => ({
   map: {
-    // width: '100%',
-    height: 480
+    width: '100%',
+    height: '480px'
   }
 });
+
+const center = {
+  lat: 45.44,
+  lng: 12.32
+};
+
+const zoom = 14;
+
+const GoogleMapExample = withGoogleMap(props => (
+  <GoogleMap defaultCenter={center} defaultZoom={zoom} {...props} />
+));
 
 /**
  * A dummy map component.
@@ -17,11 +29,12 @@ const styles = theme => ({
  * @param props
  */
 const Map = ({ classes }) => (
-  <Paper className={classes.map}>
-    <Typography variant="headline" align="center">
-      Map goes here
-    </Typography>
-  </Paper>
+  <div className={classes.map}>
+    <GoogleMapExample
+      containerElement={<Paper className={classes.map} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
+  </div>
 );
 
 Map.propTypes = {
